b46d239f46671c6a1c120e385de78494b6a81025
Rishi Raman <rraman36@github.gatech.edu>
2024-05-29 23:53:16 -0700

    FEN parsing done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use r_chess::bitboard::Bitboard;
-
 fn main() {
     println!("Hello world");
-    let position_fen = "rnbqkbnr/pp2pppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    let bitboard = position_fen.parse::<Bitboard>().unwrap();
-    let fen = bitboard.to_str();
-    println!("{}", fen);
 }
