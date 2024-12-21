e550e7bf5a0e84c339612de37f5d0c75e0750a0f
Rishi Raman <rraman36@github.gatech.edu>
2024-12-21 16:55:15 -0500

    Added board slice to square iterator

--- src/utils/board_slice.rs
+++ src/utils/board_slice.rs
@@ -1,5 +1,9 @@
 use std::fmt;
 
+use int_enum::IntEnum;
+
+use super::enums::Square;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct BoardSlice(pub u64);
 
@@ -22,5 +26,33 @@ impl fmt::Display for BoardSlice {
         writeln!(f, "\n    a b c d e f g h")?;
         write!(f, "Number: 0x{:0x}", self.0)?;
         Ok(())
+    }
+}
+
+impl BoardSlice {
+    fn iter_square(&self) -> impl Iterator<Item = Square> {
+        let mut curr_board = self.0;
+        std::iter::from_fn(move || {
+            if curr_board == 0 {
+                None
+            } else {
+                let curr_square = curr_board.trailing_zeros();
+                curr_board &= curr_board - 1;
+                Some(Square::from_int(curr_square as u8).unwrap())
+            }
+        })
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_board_slice_iter() {
+        let board_slice = BoardSlice(0b1010101);
+        let squares: Vec<Square> = board_slice.iter_square().collect();
+        let expected_vec = vec![Square::A1, Square::C1, Square::E1, Square::G1];
+        assert_eq!(squares, expected_vec);
     }
 }
