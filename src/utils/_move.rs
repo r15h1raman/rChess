bdb0492e65855fea7246d51b68a4b9364247a256
Rishi Raman <rraman36@github.gatech.edu>
2024-12-24 00:46:00 -0500

    Added display trait for Move struct

--- src/utils/_move.rs
+++ src/utils/_move.rs
@@ -1,8 +1,29 @@
 use super::enums::{Piece, Square};
+use std::fmt::{self};
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Move {
     pub orig: Square,
     pub dest: Square,
 
     pub promotion: Option<Piece>,
+}
+
+impl fmt::Display for Move {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}{}",
+            self.orig.to_string().to_lowercase(),
+            self.dest.to_string().to_lowercase()
+        )?;
+        match self.promotion {
+            Some(Piece::Queen) => writeln!(f, "q")?,
+            Some(Piece::Rook) => writeln!(f, "r")?,
+            Some(Piece::Bishop) => writeln!(f, "b")?,
+            Some(Piece::Knight) => writeln!(f, "n")?,
+            _default => writeln!(f, "")?,
+        }
+        Ok(())
+    }
 }
